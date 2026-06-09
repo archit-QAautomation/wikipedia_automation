0b0deded5650879f64773a5584ea113aabbd5842
Archit8948 <archit8948@gmail.com>
2026-06-09 18:26:30 +0530

    add reference in config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -7,6 +8,8 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/* eslint-disable @typescript-eslint/no-require-imports *
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -22,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'],['github']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
