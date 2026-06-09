1c84aced6125cfd1116f5b1ca171d966e8e2f0fa
Archit8948 <archit8948@gmail.com>
2026-06-09 18:58:59 +0530

    added new testcase for test

--- pageobjects/homepage.ts
+++ pageobjects/homepage.ts
@@ -4,13 +4,21 @@ import { expect , type Page,type Locator } from "playwright/test";
 export class Homepage {
 //locator type defined
   readonly page :Page
+  readonly searchbox : Locator
+  readonly searchbutton :Locator
 
 // constructor 
     constructor(page :Page) {
         this.page = page ;
+        this.searchbox = page.getByRole('searchbox',{name:"search"})
+        this.searchbutton = page.locator('//fieldset//button')
          }
 
 //methods
-
-
+async homesearchinput(text:any){
+  await this.searchbox.fill(text);
+}
+async homesearchbutton(){
+  await this.searchbutton.click()
+}
 } 
--- tests/leonelmessi.spec.ts
+++ tests/leonelmessi.spec.ts
@@ -6,7 +6,7 @@ import { Homepage } from "../pageobjects/homepage";
 test.describe('Title :- automation for Topic "Leonal Messi"',()=>{
 
 
-test.beforeAll('loading the homepage', async ({page}) => {
+test.beforeEach('loading the homepage', async ({page}) => {
         await page.goto('https://www.wikipedia.org/');
         await page.waitForLoadState('domcontentloaded');
     });
@@ -14,6 +14,15 @@ test.beforeAll('loading the homepage', async ({page}) => {
 
 test('search name from hompage and confirm the title of the opened page ', async ({page}) => {
      const home = new Homepage(page);
+     await expect.soft(page).toHaveTitle('Wikipedia')
+    
+});
+test('search name from hompage and search thie name of leo messi ', async ({page}) => {
+     const home = new Homepage(page);
+     await home.homesearchinput('messi')
+     await home.homesearchbutton()
+     let name : any = await page.title()
+     expect.soft(name).toContain("Messi")
     
 });
 
